b7596f1e626e96b6948cb51a1d879e32d3ef26e8
Bishoy Gerges <bishoy_gerges@outlook.com>
2026-02-08 20:03:23 -0500

    Imporved User Class

--- Invoice/User.py
+++ Invoice/User.py
@@ -5,17 +5,21 @@ class User():
     email = ""
     phone_number = ""
     is_active = False
+    user_email_verified = False
     created_at = None
     updated_at = None
-    def __init__(self, id, first_name, last_name, email, phone_number, is_active, created_at, updated_at):
+
+    
+    def __init__(self, id, first_name, last_name, email, phone_number, is_active, user_email_verified, created_at, updated_at):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.phone_number = phone_number
         self.is_active = is_active
+        self.user_email_verified = user_email_verified
         self.created_at = created_at
-        self.updated_at = None
+        self.updated_at = updated_at
 
     def create_user(self):
         pass
@@ -38,6 +42,7 @@ class User():
         pass
     
     def user_email_verified(self):
+        self.user_email_verified = True
         pass
 
 new_user = User(1, "John", "Doe", "john.doe@example.com", "123-456-7890", True, "2023-01-01", "2023-01-01")
